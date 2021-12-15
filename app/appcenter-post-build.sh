690434adbd4c71d76efef6ad8b133c95734ea5e3
Mohammed Fadin <mfadin@sa.ibm.com>
2021-12-15 10:39:28 +0400

    IMP ERROR messages for firebase

--- app/appcenter-post-build.sh
+++ app/appcenter-post-build.sh
@@ -1,23 +1,16 @@
 if [ "$AGENT_JOBSTATUS" == "Succeeded" ]; then
-    # HOCKEYAPP_API_TOKEN={API_Token}
-    # HOCKEYAPP_APP_ID={APP_ID}
     npm version
     npm install -g firebase-tools
     echo $APPCENTER_OUTPUT_DIRECTORY
-    firebase appdistribution:distribute $APPCENTER_OUTPUT_DIRECTORY/app-release.apk  \
-    --app $FIREBASE_APPID  \
-    --token $FIREBASE_TOKEN \
-    --release-notes "first release!" --testers "08_provers_visitor@icloud.com"
-    echo "Fadin the build was successful!"
     # # Example: Upload main branch app binary to HockeyApp using the API
-    # if [ "$APPCENTER_BRANCH" == "main" ];
-    #  then
-    #     curl \
-    #     -F "status=2" \
-    #     -F "ipa=@$APPCENTER_OUTPUT_DIRECTORY/MyApps.ipa" \
-    #     -H "X-HockeyAppToken: $HOCKEYAPP_API_TOKEN" \
-    #     https://rink.hockeyapp.net/api/2/apps/$HOCKEYAPP_APP_ID/app_versions/upload
-    # else
-    #     echo "Current branch is $APPCENTER_BRANCH"
-    # fi
+    if [ "$APPCENTER_BRANCH" == "main" ];
+     then
+        firebase appdistribution:distribute $APPCENTER_OUTPUT_DIRECTORY/app-release.apk  \
+        --app $FIREBASE_APPID  \
+        --token $FIREBASE_TOKEN \
+        --release-notes "first release!" --testers "08_provers_visitor@icloud.com"
+        echo "Pushing to Firebase was successful!"
+    else
+        echo "Can't push to Firebase, the current branch is $APPCENTER_BRANCH not a Main branch"
+    fi
 fi
